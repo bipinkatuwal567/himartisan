acc45fe41aefb3e11f2024045b949303e1e2844e
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-06-15 17:01:20 +0545

    minor change in sellerform buyer page

--- src/components/SellerForm.jsx
+++ src/components/SellerForm.jsx
@@ -3,11 +3,11 @@ import { Button } from "./ui/button";
 
 const SellerForm = () => {
   return (
-    <div className="w-full mx-auto p-4  h-screen flex flex-col justify-center sm:w-2/3 md:w-3/6 lg:w-2/6">
+    <div className="w-full mx-auto p-4  h-screen flex flex-col justify-center sm:w-2/3 md:w-3/6 lg:w-2/6 grainy">
       <h2 className=" font-bold text-2xl">Register your store account!</h2>
       <form
         action="#"
-        className="bg-white p-4 rounded-lg mt-5 flex flex-col gap-y-5"
+        className="rounded-lg mt-8 flex flex-col gap-y-5"
       >
         <div className="w-full flex flex-col gap-2">
           <label className="font-semibold" htmlFor="">
