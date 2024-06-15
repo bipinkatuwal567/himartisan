8b116676d4b149c1e290d87aedc057485dbfe1b7
babish9887 <babish9887@gmail.com>
2024-06-15 20:05:54 +0545

    Added logic for buyer and seller registration

--- src/components/SellerForm.jsx
+++ src/components/SellerForm.jsx
@@ -1,19 +1,61 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import { Button } from "./ui/button";
-
+import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
+import axios from 'axios'
+import toast from "react-hot-toast";
 const SellerForm = () => {
+
+      const [isLoading, setIsLoading]=useState(false)
+      const [store, setStore]=useState("")
+      const [storeDesc,setStoreDesc]=useState("")
+      const [address, setAddress]=useState("")
+      const {user}=useKindeBrowserClient()
+      const [number, setNumber]=useState("")
+
+      const handleSubmit=async()=>{
+            if(number.length!==10)
+                  return console.log("Number should be 10 digit")
+      
+            setIsLoading(true)
+            const toastid=toast.loading("Registering Seller...");
+            const name=user.given_name +" "+ user.family_name
+            const email=user.email
+            console.log(name, email)
+            try {
+                  const res=await axios.post('/api/register/seller', {name, email, contactNo:number, storeName:store,storeDescription:storeDesc, storeAddress:address}).then((res)=>{
+                        console.log(res)
+                        if(res.data.success){
+                              toast.success("Seller Registered", {id:toastid})
+                        }
+                        else{
+                              toast.error(res.data.message, {id:toastid})
+                        }
+                  })
+      
+            } catch (error) {
+                  toast.error("Something went Wrong!", {id:toastid})
+            } finally{
+                  setIsLoading(false);
+            }
+      
+      }
+
+
   return (
     <div className="w-full mx-auto p-4  h-screen flex flex-col justify-center sm:w-2/3 md:w-3/6 lg:w-2/6">
       <h2 className=" font-bold text-2xl">Register your store account!</h2>
       <form
-        action="#"
         className="bg-white p-4 rounded-lg mt-5 flex flex-col gap-y-5"
       >
+           
         <div className="w-full flex flex-col gap-2">
           <label className="font-semibold" htmlFor="">
             Store's Name
           </label>
           <input
+          value={store}
+          onChange={(e)=>setStore(e.target.value)}
             type="text"
             className="border-2 border-gray-200 outline-none p-2 rounded-md focus:border-gray-300"
           />
@@ -24,6 +66,8 @@ const SellerForm = () => {
             Store Description
           </label>
           <input
+          value={storeDesc}
+          onChange={(e)=>setStoreDesc(e.target.value)}
             type="text"
             className="border-2 border-gray-200 outline-none p-2 rounded-md focus:border-gray-300"
           />
@@ -34,12 +78,26 @@ const SellerForm = () => {
             Address
           </label>
           <input
+          value={address}
+          onChange={(e)=>setAddress(e.target.value)}
             type="text"
             className="border-2 border-gray-200 outline-none p-2 rounded-md focus:border-gray-300"
           />
         </div>
 
-        <Button>Submit</Button>
+        <div className="w-full flex flex-col gap-2">
+          <label className="font-semibold" htmlFor="">
+            Contact No
+          </label>
+          <input
+          value={number}
+          onChange={(e)=>setNumber(e.target.value)}
+            type="text"
+            className="border-2 border-gray-200 outline-none p-2 rounded-md focus:border-gray-300"
+          />
+        </div>
+
+        <Button type="button" onClick={handleSubmit}>Submit</Button>
       </form>
     </div>
   );
