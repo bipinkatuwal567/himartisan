a0783f01b77d325bd91405e81660b0add48cefbb
babish9887 <babish9887@gmail.com>
2024-06-16 07:42:51 +0545

    Fixed cart and payment bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+      images: {
+            remotePatterns: [
+              {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '**',
+              },
+              {
+                  protocol: 'https',
+                  hostname: 'firebasestorage.googleapis.com',
+                  pathname: '**',
+                },
+                {
+                  protocol: 'https',
+                  hostname: 'res.cloudinary.com',
+                  pathname: '**',
+                },
+            ],
+          },
+};
 
 export default nextConfig;
